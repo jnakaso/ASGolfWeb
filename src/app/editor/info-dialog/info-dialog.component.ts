52c40cfd85a3c5c4f1f0d2cdc3287a1013f6b47f
jnakaso <jnakaso@yahoo.com>
2022-06-11 09:48:28 -0700

    tee selection

--- src/app/editor/info-dialog/info-dialog.component.ts
+++ src/app/editor/info-dialog/info-dialog.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Inject, LOCALE_ID } from '@angular/core';
 import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ASTournament } from '../../golf/astournament';
-import { ASCourse } from '../../golf/ascourse';
+import { ASCourse, ASCourseTee } from '../../golf/ascourse';
 import { CoursesService } from '../../golf/courses.service';
 import { formatDate } from '@angular/common';
 
@@ -12,10 +12,13 @@ import { formatDate } from '@angular/common';
 })
 export class InfoDialogComponent implements OnInit {
 
+  courses: ASCourse[] = [];
+  courseTees: ASCourseTee[] = [];
   info: ASTournament;
   playDate: string;
-  selected: ASCourse
-  courses = [];
+  selected: ASCourse;
+  selectedTees: ASCourseTee;
+
   constructor(
     public modal: NgbActiveModal,
     private coursesService: CoursesService,
@@ -24,14 +27,39 @@ export class InfoDialogComponent implements OnInit {
   ngOnInit() {
     this.playDate = formatDate(this.info.date, 'yyyy-MM-dd', this.locale);
     this.coursesService.getCourses()
-      .subscribe(cc => this.courses = cc.sort((c1, c2) => c1.name.localeCompare(c2.name)));
+      .subscribe(cc => {
+        this.courses = cc.sort((c1, c2) => c1.name.localeCompare(c2.name));
+        const found = this.courses.find(c => c.id === this.info.courseId);
+        this.changeSelectedCourse(found ? found : cc[0]);
+      });
   }
 
   changeCourse(event) {
-    const course: ASCourse = this.courses.find(cc => cc.name === event);
-    this.info.courseName = course.name;
-    this.info.rating = Math.trunc(course.tees[0].rating);
-    this.info.slope = course.tees[0].slope;
+    this.changeSelectedCourse(this.courses.find(c => c.id === Number(event)));
+  }
+
+  changeSelectedCourse(aCourse: ASCourse) {
+    this.selected = aCourse;
+    if (this.selected) {
+      this.courseTees = this.selected.tees;
+      this.info.courseId = this.selected.id;
+      this.info.courseName = this.selected.name;
+      this.info.tees = this.selected.tees;
+      this.changeSelectedCourseTees(this.selected.tees[0]);
+    }
+  }
+
+  changeCourseTee(event) {
+    this.changeSelectedCourseTees(this.courseTees.find(t => t.name === event));
+  }
+
+  changeSelectedCourseTees(tees) {
+    this.selectedTees = tees;
+    if (this.selectedTees) {
+      this.info.rating = Math.trunc(tees.rating);
+      this.info.slope = tees.slope;
+      this.info.tees = tees;
+    }
   }
 
   findTee(course) {
@@ -47,7 +75,5 @@ export class InfoDialogComponent implements OnInit {
     this.info.date = new Date(this.playDate);
     this.modal.close(this.info);
   }
-
-
 
 }
--- src/app/golf/astournament.ts
+++ src/app/golf/astournament.ts
@@ -1,19 +1,25 @@
+import { ASCourseTee } from "./ascourse";
+
 export class ASTournament {
     id: string;
     date: Date = new Date();
+    courseId: number;
     courseName: string;
     seasonID: number;
     rating: number;
     slope: number;
     type: string;
+    tees: ASCourseTee[];
 
     constructor (t : any) {
         this.id = t.id;
         this.date = t.date;
+        this.courseId = t.courseId;
         this.courseName = t.courseName;
         this.seasonID = t.seasonID;
         this.rating = t.rating;
         this.slope = t.slope;
         this.type = t.type;
+        this.tees = t.tees;
     }
 }
