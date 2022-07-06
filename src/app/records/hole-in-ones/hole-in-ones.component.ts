de96b786d2d7ca85e379e30a7549d2b22911dc6c
jnakaso <jnakaso@yahoo.com>
2022-07-05 20:06:05 -0700

    tournament-single

--- src/app/records/hole-in-ones/hole-in-ones.component.ts
+++ src/app/records/hole-in-ones/hole-in-ones.component.ts
@@ -13,10 +13,17 @@ export class HoleInOnesComponent implements OnChanges {
 
 
   ngOnChanges(changes) {
-    if (this.data) {
-      this.sorted = this.data.sort((e1, e2) =>
-        new Date(e2['playDate']).getTime() - new Date(e1['playDate']).getTime());
+    if (changes.data) {
+      this.sorted = this.data
+        .map(e => Object.assign(e, { date: this.parse(e.playDate) }))
+        .sort((e1, e2) => e1.date.getTime() - e2.date.getTime());
     }
   }
 
+  parse(str: string) {
+    const parts = str.split("-");
+    return new Date(parseInt(parts[2], 10),
+      parseInt(parts[0], 10) - 1,
+      parseInt(parts[1], 10))
+  }
 }
--- src/app/tournaments/tournament-single/tournament-single.component.ts
+++ src/app/tournaments/tournament-single/tournament-single.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ASTournament } from '../../golf/astournament';
+import { TournamentsService } from '../../golf/tournaments.service';
 
 @Component({
   selector: 'as-tournament-single',
@@ -10,17 +12,28 @@ export class TournamentSingleComponent implements OnInit {
 
   season: number;
   tournamentId: number;
+  tournament;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+    private tournamentsService: TournamentsService) { }
 
   ngOnInit() {
     let sub = this.route.params.subscribe(params => {
       this.season = +params['season']; // (+) converts string 'id' to a number
       this.tournamentId = +params['id']; // (+) converts string 'id' to a number
-
-      console.log(this.season, this.tournamentId);
+      this.loadData();
 
       // In a real app: dispatch action to load the details here.
     });
   }
+
+  loadData() {
+    this.tournamentsService
+      .getTournament(this.season, this.tournamentId)
+      .subscribe(tt =>{
+        this.tournament = tt;
+        console.log(tt)
+      } );
+  }
+
 }
